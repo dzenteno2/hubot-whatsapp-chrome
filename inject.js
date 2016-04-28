b7a07b7b7021dbf8c339ecf997ea6f27126206c3
Enrique Daniel Zenteno Jiménez <daniel@holagus.com>
2016-04-28 10:18:09 -0500

    Whatsapp logger

--- inject.js
+++ inject.js
@@ -5,6 +5,7 @@ var ids = {};
 var processMessages = function() {
   var newMessages = [];
   var bubbles = $$('.bubble-text');
+  var chatTitle = $$('.pane-header .chat-title')
 
   for(var i = bubbles.length - 1; i >= 0; i--) {
     var bubble = $$(bubbles[i]);
@@ -16,11 +17,16 @@ var processMessages = function() {
     ids[reactid] = true;
 
     var text = bubble.find('.message-text .emojitext').text();
+    var user = chatTitle.find('.emojitext').text();
+    var messageOut = bubble.parent().hasClass('message-out');
+    var time = bubble.find('.message-meta .message-datetime').text();
 
     newMessages.push({
       reactid: reactid,
       text: text,
-
+      user: user,
+      messageOut: messageOut,
+      time: time
     });
   }
 
@@ -36,24 +42,12 @@ setInterval(function() {
   var newMessages = processMessages();
   for (newMessage of newMessages) {
     messages.unshift(newMessage);
-    var text = newMessage.text
-    console.log("New message, posting to extension: " + text);
+    var text = newMessage.text;
+    var user = newMessage.user;
+    var direction = newMessage.messageOut ? "output" : "input";
+    var time = newMessage.time;
+    console.log("New "+ direction +" message from "+ user +" at "+ time +", posting to extension: " + text);
     port.postMessage({message: text});
   }
-}, 500)
-
-port.onMessage.addListener(function(data) {
-  var msg = data.message;
-  console.log("Received reply from extension: " + msg);
-  $$('div.input').html(msg);
-  
-  var event = document.createEvent('Event');
-  event.initEvent('input', true, true);
-  $$('div.input')[0].dispatchEvent(event);
-
-  setTimeout(function() {
-    $$('.icon-send').click();
-  }, 50);
-});
-
+}, 5000)
 
